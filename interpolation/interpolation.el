91820ed5bfc9849b5ea5ac00c61833db5f7b4a48
asnr <asnr@users.noreply.github.com>
2018-01-25 22:18:17 +0800

    Add more detail to comments

--- interpolation/interpolation.el
+++ interpolation/interpolation.el
@@ -5,7 +5,7 @@
 ;;
 ;;   "This is a string ${some-value-to-interpolate}"
 ;;
-;; Notably, we allow strings to span multiple lines.
+;; Notably, we allow strings and string interpolation to span multiple lines.
 
 ;; We will let the syntax table take care of highlighting strings and will
 ;; highlight the interpolated regions with `font-lock-defaults' and
@@ -17,4 +17,6 @@
 
 (define-derived-mode interpolation-mode fundamental-mode "interpolation"
   (setq font-lock-defaults '(interpolation-highlights))
+  ;; font-lock-multiline is makes emacs recompute highlighting on edits inside
+  ;; an interpolation spanning multiple lines
   (setq font-lock-multiline t))
